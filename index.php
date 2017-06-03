46cc4addc73c9aa3088bd6f02deac7da7f76be46
Isaac Diamond <isaacd9@gmail.com>
2017-06-03 14:21:14 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php if (!($_SERVER["HTTPS"])) {
+  $redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+  header("HTTP/1.1 301 Moved Permanently");
+  header("Location: $redirect");
+} ?>
+
+<html>
+<head>
+<title>Isaac Diamond</title>
+
+<style>a {font-size: 1.1em}</style>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new
+ Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-72992612-1', 'auto');
+ga('send', 'pageview');
+
+</script>
+</head>
+
+<body style="padding: 25px;">
+<div>
+<h1> It works! Welcome to Isaac's website. </h1>
+
+<h2> The current time is: <?php echo(date("g:i:s a")); ?>, GMT. 
+  <?php date_default_timezone_set('America/Los_Angeles'); 
+  echo(date("g:i:s a")); ?>, PST</h2>
+<h2> You are visiting from: <?php echo($_SERVER["REMOTE_ADDR"])?> </h2>
+
+<a href="https://www.facebook.com/isaac.diamond.5">Facebook</a>
+
+<a href="https://linkedin.com/in/isaacdiamond">LinkedIn</a>
+
+<a href="https://twitter.com/isaacinthesky">Twitter</a>
+
+<a href="https://github.com/isaacd9/">GitHub</a>
+
+<a href="https://medium.com/@isaacdiamond">Medium</a>
+
+<a href="https://idd.photography">Photos</a>
+
+<a href="https://keybase.io/idiamond">Keybase</a>
+
+<a href="resume.pdf">Resume</a>
+
+<a href="pubkey.php">PGP</a>
+
+<br>
+<br>
+
+
+<span style="font-size: .5em">
+Copyright (c) 2017 Isaac Diamond. Verbatim copying and distribution of this
+entire article are permitted worldwide, without royalty, in any medium,
+provided this notice is preserved.
+</span>
+
+<br>
+<br>
+
+<a href="https://www.eff.org/files/2016/11/01/2017mb.png"><img height=20px
+src="https://www.eff.org/files/2016/11/01/2017mb.png" /></a>
+
+<a href="aclu.png"><img height=20px src="aclu.png" /></a>
+
+</div>
+</body>
+</html>
+
---
+++ pubkey.php
@@ -0,0 +1,51 @@
+<?php if (!($_SERVER["HTTPS"])) {
+  $redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+  header("HTTP/1.1 301 Moved Permanently");
+  header("Location: $redirect");
+} ?>
+
+<html>
+<head>
+<title>Isaac Diamond</title>
+
+<body style="padding: 25px;">
+<a>isaacd9[at]gmail.com</a>
+<pre>
+FINGERPRINT:
+FC15 E51C BCBD EE6B 65B1  5E81 A223 5C1F 9CE0 AC3E
+</pre>
+
+<pre>
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: GnuPG v2
+
+mQENBFiWRY4BCACz39odCht0OTatLRt8vJhM1IYaPRX0yguP3AL1ih+bGn2mGIeJ
+YZo396aPVOlcdMhW3KHyGTBYLP9AiUVNHYZihWyKZrg8Ciul+zU6YP3Ee9pQaykn
+eM3Y9solYwoJlNHY7F6naaS23ytd8Xjz4tMzF5n32VnSRPSK+Z99VCZoJEGzgwMl
+AJrLCxHFh6MdivaSoViEJRCUCUW2nQ4Gld+1u8n/Fd7Zjgk2+uI06dDbBv+Ze4XC
+rWWuCDucUChEXZK+YiPU9++Bu4w5c0XviO/imrkZtO7vOe+DzyrCC7N3N5CfQHSq
+00S34DrffJwcKo4/vEJTrdVOxruEp6ItsxwHABEBAAG0IUlzYWFjIERpYW1vbmQg
+PGlzYWFjZDlAZ21haWwuY29tPokBOQQTAQgAIwUCWJZFjgIbAwcLCQgHAwIBBhUI
+AgkKCwQWAgMBAh4BAheAAAoJEKIjXB+c4Kw+NDIIAIPCGtQq6RiLNEMePBsK4jwQ
+lYyvAdxo57Zcwe55ZcaqGc4tu/saaT+v1KlgF/EEnFh6i4ZqhJoKftrKYvdFpOJF
+CvJC3r5yUaNH+rKKZbIbbapMFxIyECBxlerYe/OABIhED/fmoJ0raA0XqCbHRrmT
+hnrFNNW3HpTrL2nqpRQBs8GO/jFzgQDs67RMVC3Op8KLa26P5mUOpgcUm1BMjWEj
+znvAQNfnu3jFDnhFolgzVT7KVPcPLrPWZmSOlXpY7a+wk6K3H0bWxx0Lfpj5qoVz
+uceEFabjajLJ5CgrvSuLgSey3g7XCEAKPgtIj06gXsR7bB+tgN26keehOhFZUJ+5
+AQ0EWJZFjgEIANCVyJCW9dBJUR1YeqLYzkger03l28S/AU9vxZ1YHqI3j2AicCAc
+nKsLv0hTD4lNvpB9/hH4fZgYtkYVtgn0sxt1yAbLt0vtkarg/UeP1Br7AB4a7JZm
+FT5Vh/dX4eY+Iq+DKQAwlB5BbCR+f9CI9T6Z7zCTktEjrJtpkuC0NJ6TohY0j7eZ
+otaw7M7EpcdqzuysSNf90If790k6KZY7ljvXK/ypmpYWjWCwNw9cbB17dNg8VtGq
+Euzt+tp3fsx6zvyiXQ3pQqoq0TIy9n6or9SxGz8ZlZYFh9xp8tRJS060Qbf7aXLL
+91ShbtXq/S/DywTsvtaoV26yj+BVZupr+kUAEQEAAYkBHwQYAQgACQUCWJZFjgIb
+DAAKCRCiI1wfnOCsPgnzCACdWioLGXn1Mz4+sdMhhoXf++UgBRkthWSoUyRkQv7b
+1bW4Jtzfhq7yC7CJIsInzWn38zoFOYYRq4UTSQF9qsp48ir4TrikDNFGIfQj/qbv
+luNKi4sa+FWXBIelOvyzfVkHYetAufK/z6Mq+5D9rsHEVkK9PXuVMkgOGyzqlo/c
+avVimeq+Rfu2UbExNL9qggAwQIgNU+WPE+yjTDuyxOzcaLD3kofh468wv8UhMaid
+3KNPDc+cyXo+WdR5i1l72mPSRYcfuH/x6CcYtuQGSLNBe5eBXnyeAUlZvwzeQSNz
+367tU5Cw1JkuQrAKubMtGVO5Y6fF1orOzX1+d6ejYgL1
+=oScz
+-----END PGP PUBLIC KEY BLOCK-----
+</pre>
+
+<a href="pubkey.txt">Direct Link</a>
